a71ea64cfce2d60761367d15fe6832dae641e3bc
shivaji <shivaji.chate@anarsolutions.com>
2022-06-30 00:04:53 +0530

    fresh data

--- prod_run.sh
+++ prod_run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./cloud_sql_proxy -dir=/cloudsql -instances=charged-skein-354804:us-central1:dagster=tcp:34.135.204.152:5432 &
+./cloud_sql_proxy -dir=/cloudsql -instances=charged-skein-354804:us-central1:dagster=tcp:34.121.245.163:5432 &
 sleep 10
 dagster-daemon run &
 dagit -h 0.0.0.0 -p 3000 -w /opt/dagster/app/workspace.yaml
